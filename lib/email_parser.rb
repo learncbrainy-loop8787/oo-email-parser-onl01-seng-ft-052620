dc7b71b8836100e88adcfb61951fc39c8a317db6
korey ko <koreykendrick1@gmail.com>
2020-06-06 01:47:15 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 class EmailParser 
-  attr_accessor :name, :csv_emails
+  attr_accessor :csv_emails
   
   def initialize(csv_emails)
 @csv_emails = csv_emails.parse
